11a17f9fea5da52c662d2fabf3e92dc09b87de6d
Tyler Norlund <tnorlund@icloud.com>
2023-01-29 12:28:25 -0800

    💯 Testing Point2D
    Wrote unit tests to cover the Point2D class.

--- src/Point2D.ts
+++ src/Point2D.ts
@@ -7,24 +7,32 @@ export default class Point2D {
   }
 
   /**
-   * Translate this point by that point
+   * Adds a 2-dimensional point to this 2-dimensional point
    *
    * @param {Point2D} that
+   * @returns The result of adding this 2-dimensional point with that
    */
   public add(that: Point2D): Point2D {
-    return this.constructor(this.x + that.x, this.y + that.y);
-  }
-
-  public subtract(that: Point2D) {
-    return this.constructor(this.x - that.x, this.y - that.y);
+    return new Point2D(this.x + that.x, this.y + that.y);
   }
 
   /**
+   * Subtracts a 2-dimensional point to this 2-dimensional point
    *
+   * @param {Point2D} that
+   * @returns The result of subtracting this 2-dimensional point from that
+   */
+  public subtract(that: Point2D) {
+    return new Point2D(this.x - that.x, this.y - that.y);
+  }
+
+  /**
+   * Multiplies this 2-dimensional point by a scalar
+   * 
    * @param {number} scalar
    */
   public multiply(scalar: number): Point2D {
-    return this.constructor(this.x * scalar, this.y * scalar);
+    return new Point2D(this.x * scalar, this.y * scalar);
   }
 
   /**
@@ -35,6 +43,3 @@ export default class Point2D {
     return `point(${this.x},${this.y})`;
   }
 }
-
-// module.exports = { Point2D }
-// export const Point2D: any;
--- src/__tests__/test-point2d.ts
+++ src/__tests__/test-point2d.ts
@@ -1,11 +1,42 @@
-import {Point2D} from "../../index";
-import {describe, expect, test} from '@jest/globals';
-
+import { Point2D } from "../../index";
+import { describe, expect, test } from "@jest/globals";
 
 describe("testing Point2D", () => {
   test("No arguments should default to 0, 0", () => {
     const point = new Point2D();
     expect(point.x).toBe(0);
     expect(point.y).toBe(0);
+  });
+
+  test("Given arguments are in x, y order", () => {
+    const point = new Point2D(1, 2);
+    expect(point.x).toBe(1);
+    expect(point.y).toBe(2);
+  });
+
+  test("One point can be added to another point", () => {
+    const point_1 = new Point2D(1, 2);
+    const point_2 = new Point2D(3, 4);
+    const point_result = new Point2D(4, 6);
+    expect(point_1.add(point_2)).toEqual(point_result);
+  });
+
+  test("One point can be subtracted from another point", () => {
+    const point_1 = new Point2D(3, 2);
+    const point_2 = new Point2D(1, 2);
+    const point_result = new Point2D(2, 0);
+    expect(point_1.subtract(point_2)).toEqual(point_result);
+  });
+
+  test("One point can be multiplied by a scalar", () => {
+    const point = new Point2D(3, 2);
+    const scalar = 3
+    const point_result = new Point2D(9, 6);
+    expect(point.multiply(scalar)).toEqual(point_result);
+  });
+
+  test("The point can be represented as a string", () => {
+    const point = new Point2D(3, 2);
+    expect(point.toString()).toBe("point(3,2)");
   });
 });
